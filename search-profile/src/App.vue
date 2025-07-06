1799dd4b96ec34fccf7ca6d673c831949153886e
ThanhNhu1606 <tnhu16064@gmail.com>
2025-07-06 09:05:59 +0000

    add header

---
+++ search-profile/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HeadPage from './components/HeadPage.vue'
+import MainContent from './components/MainContent.vue'
+</script>
+
+<template>
+<HeadPage/>
+<MainContent/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ search-profile/src/components/HeadPage.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+    <div class="relative">
+    <!-- Banner -->
+    <img class="w-full object-cover" src="/workspaces/github-profile/resources/hero-image-github-profile-sm.jpg" alt="Banner Profile">
+
+    <!-- Thanh tìm kiếm -->
+        <div class="absolute top-4 left-1/2 -translate-x-1/2 bg">
+        <input id="searchInput" type="search" placeholder="Tìm kiếm..." class="w-72 max-w-full px-4 py-2 rounded-full shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
